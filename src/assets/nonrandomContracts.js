58c8554f720b5c2215962ab0d9c70dd22ebce4b0
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-03-15 15:45:03 +0100

    Update example contracts

--- src/assets/nonrandomContracts.js
+++ src/assets/nonrandomContracts.js
@@ -19,18 +19,6 @@ export default [
     address: "0xe592427a0aece92de3edee1f18e0157c05861564",
   },
   {
-    name: "XMON",
-    info: "",
-    chainId: 1,
-    address: "0x3aada3e213abf8529606924d8d1c55cbdc70bf74",
-  },
-  {
-    name: "ShibaSwap",
-    info: "",
-    chainId: 1,
-    address: "0x03f7724180aa6b939894b5ca4314783b0b36b329",
-  },
-  {
     name: "PODK",
     info: "metadata on ipfs",
     chainId: 1,
@@ -38,9 +26,9 @@ export default [
   },
   {
     name: "CollateralShort",
-    info: "Optimism Testnet",
-    chainId: 69,
-    address: "0xb36c99e9a86ff467bbf4312ae852874f7a6fe57d",
+    info: "Experimental Flag",
+    chainId: 1,
+    address: "0x1F2c3a1046c32729862fcB038369696e3273a516",
   },
   {
     name: "SushiSwap LP",
@@ -50,13 +38,13 @@ export default [
   },
   {
     name: "BUSD",
-    info: "BinanceSC",
+    info: "BinanceSC, with bzzr1",
     chainId: 56,
     address: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
   },
   {
     name: "Wonderland DAO",
-    info: "Avalanche",
+    info: "Avalanche, with bzzr0",
     chainId: 43114,
     address: "0x78a9e536EBdA08b5b9EDbE5785C9D1D50fA3278C",
   },
